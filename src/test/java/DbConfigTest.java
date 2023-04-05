d465ca8ad1c487c2936b525a14049bb552e6eb3d
Tanrari <86423821+Tanrari@users.noreply.github.com>
2023-04-05 23:03:55 +0300

    #4

---
+++ src/test/java/DbConfigTest.java
@@ -0,0 +1,52 @@
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+public class DbConfigTest {
+    @Test
+    public void testOne() throws SQLException {
+        GenericApplicationContext ctx = new AnnotationConfigApplicationContext(DbConfig.class);
+        DataSource dataSource = ctx.getBean("dataSource", DataSource.class);
+        assertNotNull(dataSource);
+//        System.out.println(dataSource.getConnection().getCatalog());
+        testDataSource(dataSource);
+        ctx.close();
+
+    }
+
+    public void testDataSource(DataSource dataSource) throws SQLException {
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+            System.out.println(connection.getCatalog());
+            PreparedStatement statement = connection.prepareStatement("select * from singer where id=1");
+//            System.out.println(statement.execute());
+            ResultSet resultSet = statement.executeQuery();
+//            statement.execute();
+//            System.out.println(resultSet);
+            while (resultSet.next()){
+//                System.out.println(resultSet.);
+                 String mockVal = resultSet.getString("first_name");
+                assertEquals("John", mockVal);
+//                System.out.println(resultSet.getString("first_name"));
+            }
+//            statement.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        finally {
+            if (connection!=null){
+                connection.close();
+            }
+        }
+    }
+}
--- src/test/java/TestJdbc.java
+++ src/test/java/TestJdbc.java
@@ -1,6 +1,15 @@
+import java.sql.Date;
+import java.util.GregorianCalendar;
+
 public class TestJdbc {
     public static void main(String[] args) {
         SingerDao dao = new PlainSingerDao();
         System.out.println(dao.findAll());
+        Singer singer = new Singer();
+        singer.setBirthDate(new Date(new GregorianCalendar(1991,2,3)
+                .getTime().getTime()));
+        singer.setFirstName("Lucas");
+        singer.setLastName("Sheerah");
+        dao.insert(singer);
     }
 }
